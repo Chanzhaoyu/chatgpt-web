86fcbbff0bcba7da231c50992f8528fff7e70f34
ChenZhaoYu <790348264@qq.com>
2023-02-14 10:04:32 +0800

    feat: 上限文联想改为前端进行

--- service/src/chatgpt.ts
+++ service/src/chatgpt.ts
@@ -15,67 +15,31 @@ const apiKey = process.env.OPENAI_API_KEY
 if (apiKey === undefined)
   throw new Error('OPENAI_API_KEY is not defined')
 
-const chatContext = new Set<ChatContext>()
-
 /**
  * More Info: https://github.com/transitive-bullshit/chatgpt-api
  */
-const api = new ChatGPTAPI({ apiKey })
+const api = new ChatGPTAPI({ apiKey, debug: false })
 
-async function chatReply(message: string) {
+async function chatReply(
+  message: string,
+  lastContext?: { conversationId?: string; parentMessageId?: string },
+) {
   if (!message)
     return sendResponse({ type: 'Fail', message: 'Message is empty' })
 
   try {
-    // Get the last context from the chat context
     let options: SendMessageOptions = {}
-
-    const lastContext = Array.from(chatContext).pop()
 
     if (lastContext)
       options = { ...lastContext }
 
     const response = await api.sendMessage(message, { ...options })
 
-    const { conversationId, id } = response
-
-    // Add the new context to the chat context
-    if (conversationId && id)
-      chatContext.add({ conversationId, parentMessageId: id })
-
     return sendResponse({ type: 'Success', data: response })
   }
   catch (error: any) {
-    global.console.log(error)
     return sendResponse({ type: 'Fail', message: error.message })
   }
 }
 
-async function chatReplayOne(message: string, options?: ChatContext) {
-  if (!message)
-    return sendResponse({ type: 'Fail', message: 'Message is empty' })
-
-  try {
-    let messageOptions: SendMessageOptions = {}
-
-    if (options) {
-      const { conversationId, parentMessageId } = options
-      messageOptions = { conversationId, parentMessageId }
-
-      const response = await api.sendMessage(message, { ...messageOptions })
-
-      return sendResponse({ type: 'Success', data: response })
-    }
-  }
-  catch (error: any) {
-    return sendResponse({ type: 'Fail', message: error.message })
-  }
-}
-
-async function clearChatContext() {
-  // Clear the chat context
-  chatContext.clear()
-  return sendResponse({ type: 'Success', message: 'Chat context cleared' })
-}
-
-export { chatReply, chatReplayOne, clearChatContext }
+export { chatReply }
--- src/components/business/Chat/index.vue
+++ src/components/business/Chat/index.vue
@@ -1,51 +1,27 @@
 <script setup lang='ts'>
-import { nextTick, onMounted, ref } from 'vue'
+import { computed, nextTick, onMounted, ref } from 'vue'
 import { NButton, NInput, useMessage } from 'naive-ui'
+import type { ChatOptions, ChatProps } from './types'
 import { Message } from './components'
 import { Layout } from './layout'
-import { clearConversations, fetchChatAPI } from '@/api'
+import { fetchChatAPI } from '@/api'
 import { HoverButton, SvgIcon } from '@/components/common'
-
-interface ListProps {
-  dateTime: string
-  message: string
-  reversal?: boolean
-  error?: boolean
-}
 
 const scrollRef = ref<HTMLDivElement>()
 
 const ms = useMessage()
 
 const prompt = ref('')
-
 const loading = ref(false)
 
-const list = ref<ListProps[]>([])
-
-onMounted(initChat)
+const list = ref<ChatProps[]>([])
+const chatList = computed(() => list.value.filter(item => (!item.reversal && !item.error)))
 
 function initChat() {
-  addMessage('Hi, I am ChatGPT, a chatbot based on GPT-3.', false)
+  addMessage('Hi, I am ChatGPT, a chatbot based on GPT-3.')
 }
 
-async function handleClear() {
-  try {
-    await clearConversations()
-  }
-  catch (error) {
-    ms.error('Clear failed, please try again later.')
-  }
-  finally {
-    list.value = []
-    setTimeout(initChat, 100)
-  }
-}
-
-function handleEnter(event: KeyboardEvent) {
-  if (event.key === 'Enter')
-    handleSubmit()
-}
+onMounted(initChat)
 
 async function handleSubmit() {
   if (loading.value)
@@ -58,25 +34,50 @@ async function handleSubmit() {
     return
   }
 
-  addMessage(message, true)
+  addMessage(message, { reversal: true })
   prompt.value = ''
+
+  let options: ChatOptions = {}
+  const lastContext = chatList.value[chatList.value.length - 1]?.options
+
+  if (lastContext)
+    options = { ...lastContext }
 
   try {
     loading.value = true
-    const { data } = await fetchChatAPI(message)
-    addMessage(data?.text ?? '', false)
+    const { data } = await fetchChatAPI(message, options)
+    addMessage(data?.text ?? '', { options: { conversationId: data.conversationId, parentMessageId: data.id } })
   }
   catch (error: any) {
-    addMessage(`Error: ${error.message ?? 'Request failed, please try again later.'}`, false, true)
+    addMessage(`Error: ${error.message ?? 'Request failed, please try again later.'}`, { error: true })
   }
   finally {
     loading.value = false
   }
 }
 
-function addMessage(message: string, reversal = false, error = false) {
-  list.value.push({ dateTime: new Date().toLocaleString(), message, reversal, error })
+function handleEnter(event: KeyboardEvent) {
+  if (event.key === 'Enter')
+    handleSubmit()
+}
+
+function addMessage(
+  message: string,
+  args?: { reversal?: boolean; error?: boolean; options?: ChatOptions },
+) {
+  list.value.push({
+    dateTime: new Date().toLocaleString(),
+    message,
+    reversal: args?.reversal ?? false,
+    error: args?.error ?? false,
+    options: args?.options ?? undefined,
+  })
   nextTick(() => scrollRef.value && (scrollRef.value.scrollTop = scrollRef.value.scrollHeight))
+}
+
+function handleClear() {
+  list.value = []
+  setTimeout(initChat, 100)
 }
 </script>
 
@@ -87,12 +88,8 @@ function addMessage(message: string, reversal = false, error = false) {
         <div ref="scrollRef" class="h-full p-4 overflow-hidden overflow-y-auto">
           <div>
             <Message
-              v-for="(item, index) of list"
-              :key="index"
-              :date-time="item.dateTime"
-              :message="item.message"
-              :reversal="item.reversal"
-              :error="item.error"
+              v-for="(item, index) of list" :key="index" :date-time="item.dateTime" :message="item.message"
+              :reversal="item.reversal" :error="item.error"
             />
           </div>
         </div>
