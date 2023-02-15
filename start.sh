30d734a214226081035bb550d529bae118c5ae17
voidm <marlkiller@voidm.com>
2023-02-15 21:41:52 +0800

    Fixed : Docker log file

--- start.sh
+++ start.sh
@@ -3,8 +3,9 @@ cd ./service
 nohup pnpm start > service.log &
 echo "Start service complete!"
 
+
 cd ..
+echo "" > front.log
 nohup pnpm dev > front.log &
 echo "Start front complete!"
-
 tail -f front.log
