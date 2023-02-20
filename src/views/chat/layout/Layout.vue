cb90d81c69f7b7f26962b125e5ef11e0291467ca
ChenZhaoYu <790348264@qq.com>
2023-02-20 14:58:06 +0800

    fix: 默认 `active` 赋值

--- src/views/chat/layout/Layout.vue
+++ src/views/chat/layout/Layout.vue
@@ -1,12 +1,17 @@
 <script setup lang='ts'>
 import { computed } from 'vue'
 import { NLayout, NLayoutContent } from 'naive-ui'
+import { useRouter } from 'vue-router'
 import Sider from './sider/index.vue'
 import Header from './header/index.vue'
 import { useBasicLayout } from '@/hooks/useBasicLayout'
-import { useAppStore } from '@/store'
+import { useAppStore, useChatStore } from '@/store'
 
+const router = useRouter()
 const appStore = useAppStore()
+const chatStore = useChatStore()
+
+router.replace({ name: 'Chat', params: { uuid: chatStore.active } })
 
 const { isMobile } = useBasicLayout()
 
