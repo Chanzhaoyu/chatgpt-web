3563a12c839839960c5e1c6b67a936ae6e28086e
ChenZhaoYu <790348264@qq.com>
2023-02-22 08:46:09 +0800

    perf: 检查代码

--- src/utils/functions/includeCode.ts
+++ src/utils/functions/includeCode.ts
@@ -1,8 +1,6 @@
 function includeCode(text: string | null | undefined) {
   const regexp = /^(?:\s{4}|\t).+/gm
-  if (text?.includes(' = ') || text?.match(regexp))
-    return true
-  return false
+  return !!(text?.includes(' = ') || text?.match(regexp))
 }
 
 export default includeCode
