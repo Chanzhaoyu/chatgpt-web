80d77663a793589745f44220215ebc10fd705d2e
ChenZhaoYu <790348264@qq.com>
2023-02-23 11:14:17 +0800

    feat: 更新版本查看

--- src/components/common/Setting/index.vue
+++ src/components/common/Setting/index.vue
@@ -1,6 +1,7 @@
 <script setup lang='ts'>
 import { computed, ref, watch } from 'vue'
 import { NCard, NModal } from 'naive-ui'
+import pkg from '../../../../package.json'
 import { fetchChatConfig } from '@/api'
 
 interface Props {
@@ -55,9 +56,16 @@ watch(
   <NModal v-model:show="show" style="width: 80%; max-width: 460px;">
     <NCard>
       <div class="space-y-4">
-        <h1 class="text-xl font-bold">
-          当前后台设置
-        </h1>
+        <h2 class="text-xl font-bold text-center">
+          Version - {{ pkg.version }}
+        </h2>
+        <hr>
+        <p>
+          此项目开源于
+          <a class="text-blue-600" href="https://github.com/Chanzhaoyu/chatgpt-web" target="_blank">Github</a>
+          ，免费并且协议为 MIT，其他来源均为盗版，使用时请注意。如果你觉得此项目对你有帮助，请帮我点个 Star，谢谢！
+        </p>
+        <hr>
         <p>API方式：{{ config?.apiModel ?? '-' }}</p>
         <p>反向代理：{{ config?.reverseProxy ?? '-' }}</p>
         <p>超时时间：{{ config?.timeoutMs ?? '-' }}</p>
