fe55650af9a6bbbc7f1ca55fb776f39010472052
ChenZhaoYu <790348264@qq.com>
2023-02-25 22:22:14 +0800

    fix: 修复样式问题[#123][#126]

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -345,7 +345,7 @@ onUnmounted(() => {
 <template>
   <div class="flex flex-col h-full" :class="wrapClass">
     <main class="flex-1 overflow-hidden">
-      <div ref="scrollRef" class="h-full p-4 overflow-hidden overflow-y-auto" :class="[{ 'p-2': isMobile }]">
+      <div ref="scrollRef" class="h-full overflow-hidden overflow-y-auto" :class="[isMobile ? 'p-2' : 'p-4']">
         <template v-if="!dataSources.length">
           <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
             <SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" />
