dd129fb32bcc81c334217b2a05b7f5bfc83adefb
ChenZhaoYu <790348264@qq.com>
2023-02-26 10:05:33 +0800

    feat: 移动端标题返回顶部和返回底部

--- src/store/modules/chat/index.ts
+++ src/store/modules/chat/index.ts
@@ -6,6 +6,13 @@ export const useChatStore = defineStore('chat-store', {
   state: (): Chat.ChatState => getLocalState(),
 
   getters: {
+    getChatHistoryByCurrentActive(state: Chat.ChatState) {
+      const index = state.history.findIndex(item => item.uuid === state.active)
+      if (index !== -1)
+        return state.history[index]
+      return null
+    },
+
     getChatByUuid(state: Chat.ChatState) {
       return (uuid?: number) => {
         if (uuid)
@@ -66,6 +73,18 @@ export const useChatStore = defineStore('chat-store', {
     async setActive(uuid: number) {
       this.active = uuid
       return await this.reloadRoute(uuid)
+    },
+
+    getChatByUuidAndIndex(uuid: number, index: number) {
+      if (!uuid || uuid === 0) {
+        if (this.chat.length)
+          return this.chat[0].data[index]
+        return null
+      }
+      const chatIndex = this.chat.findIndex(item => item.uuid === uuid)
+      if (chatIndex !== -1)
+        return this.chat[chatIndex].data[index]
+      return null
     },
 
     addChatByUuid(uuid: number, chat: Chat.Chat) {
