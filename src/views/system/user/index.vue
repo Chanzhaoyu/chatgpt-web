0c0bba9ade10618907d12bc372a5b94231fc1645
ChenZhaoYu <790348264@qq.com>
2023-02-28 20:48:05 +0800

    feat: 个人中心

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -1,5 +1,6 @@
 <script setup lang='ts'>
-
+import { NTabPane, NTabs } from 'naive-ui'
+import { SvgIcon } from '@/components/common'
 </script>
 
 <template>
@@ -8,6 +9,36 @@
       <h2 class="py-6 mb-4 text-2xl font-bold">
         个人中心
       </h2>
+      <NTabs type="line" animated>
+        <NTabPane name="General" tab="总览">
+          <template #tab>
+            <SvgIcon class="text-lg" icon="ri:file-user-line" />
+            <span class="ml-2">总览</span>
+          </template>
+          <span>General</span>
+        </NTabPane>
+        <NTabPane name="Billing" tab="账单">
+          <template #tab>
+            <SvgIcon class="text-lg" icon="ri:bill-line" />
+            <span class="ml-2">账单</span>
+          </template>
+          <span>Billing</span>
+        </NTabPane>
+        <NTabPane name="Notification" tab="通知">
+          <template #tab>
+            <SvgIcon class="text-lg" icon="ri:notification-4-line" />
+            <span class="ml-2">通知</span>
+          </template>
+          <span>Notification</span>
+        </NTabPane>
+        <NTabPane name="ChangePassword" tab="修改密码">
+          <template #tab>
+            <SvgIcon class="text-lg" icon="ri:key-line" />
+            <span class="ml-2">修改密码</span>
+          </template>
+          <span>Change Password</span>
+        </NTabPane>
+      </NTabs>
     </div>
   </div>
 </template>
