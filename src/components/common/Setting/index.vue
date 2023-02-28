bcb9a728589f64f06f2cb4c8af323d3df1303648
WIN-2NLMPPSJBCJ\Administrator <15574884690@163.com>
2023-02-28 21:47:13 +0800

    feat: bug修复

--- src/components/common/Setting/index.vue
+++ src/components/common/Setting/index.vue
@@ -1,7 +1,6 @@
 <script setup lang='ts'>
 import { computed, ref, watch } from 'vue'
 import { NCard, NModal } from 'naive-ui'
-import pkg from '../../../../package.json'
 import { fetchChatConfig } from '@/api'
 
 interface Props {
@@ -58,15 +57,21 @@ watch(
     <NCard>
       <div class="space-y-4">
         <h2 class="text-xl font-bold text-center">
-          Version - {{ pkg.version }}
+          ChatGPT - web
         </h2>
         <hr>
         <p>
           此项目为个人项目，仅限学习！如果你觉得此项目对你有所帮助，请关注下方公众号，谢谢！
         </p>
         <hr>
-        <img v-if="image" src="@/assets/qrcode.jpg" class="object-cover w-full h-full " alt="avatar">
+        <img src="@/assets/qrcode.jpg" class="icon" alt="avatar">
       </div>
     </NCard>
   </NModal>
 </template>
+
+<style>
+.icon {
+  width: 125px;
+}
+</style>
