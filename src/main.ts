0e5a295e8cc429679d5131d8a943887dd08b7779
whatwewant <tobewhatwewant@outlook.com>
2023-03-05 01:28:16 +0800

    feat: add on page show check user

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { setupStore } from './store'
 import { setupRouter } from './router'
 
 import { useUserStore } from '@/store'
+import doreamon from '@zodash/doreamon'
 
 async function bootstrap() {
   const app = createApp(App)
@@ -20,11 +21,93 @@ async function bootstrap() {
 
   await setupRouter(app)
 
-  const userStore = useUserStore()
-
-  userStore.setupUserInfo()
+  await onSetup(app);
 
   app.mount('#app')
 }
 
 bootstrap()
+
+const onSetup = async (app: App) => {
+  // setup user
+  const userStore = useUserStore();
+  await userStore.setupUserInfo();
+
+  // setup on page show => user login
+  doreamon.dom.page.onPageShow(async () => {
+    const response = await fetch('/api/user');
+    if (response.status === 401) {
+      const rootSelector = document.querySelector('#app');
+
+      rootSelector!.innerHTML = `
+<style>
+.loading-wrap {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.balls {
+  width: 4em;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.balls div {
+  width: 0.8em;
+  height: 0.8em;
+  border-radius: 50%;
+  background-color: #4b9e5f;
+}
+
+.balls div:nth-of-type(1) {
+  transform: translateX(-100%);
+  animation: left-swing 0.5s ease-in alternate infinite;
+}
+
+.balls div:nth-of-type(3) {
+  transform: translateX(-95%);
+  animation: right-swing 0.5s ease-out alternate infinite;
+}
+
+@keyframes left-swing {
+
+  50%,
+  100% {
+    transform: translateX(95%);
+  }
+}
+
+@keyframes right-swing {
+  50% {
+    transform: translateX(-95%);
+  }
+
+  100% {
+    transform: translateX(100%);
+  }
+}
+
+@media (prefers-color-scheme: dark) {
+  body {
+    background: #121212;
+  }
+}
+</style>
+<div class="loading-wrap">
+<div class="balls">
+  <div></div>
+  <div></div>
+  <div></div>
+</div>
+</div>`;
+
+      await doreamon.delay(300);
+
+      window.location.reload();
+    }
+  })
+}
