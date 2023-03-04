7fd3899433cd4de941156158e4a265b5743c4ee2
ChenZhaoYu <790348264@qq.com>
2023-03-04 08:52:01 +0800

    perf: highlight.js 新语法警告

--- src/views/chat/components/Message/Text.vue
+++ src/views/chat/components/Message/Text.vue
@@ -30,7 +30,7 @@ renderer.code = (code, language) => {
   const validLang = !!(language && hljs.getLanguage(language))
   if (validLang) {
     const lang = language ?? ''
-    return `<pre class="code-block-wrapper"><div class="code-block-header"><span class="code-block-header__lang">${lang}</span><span class="code-block-header__copy">${t('chat.copyCode')}</span></div><code class="hljs code-block-body ${language}">${hljs.highlight(lang, code).value}</code></pre>`
+    return `<pre class="code-block-wrapper"><div class="code-block-header"><span class="code-block-header__lang">${lang}</span><span class="code-block-header__copy">${t('chat.copyCode')}</span></div><code class="hljs code-block-body ${language}">${hljs.highlight(code, { language: lang }).value}</code></pre>`
   }
   return `<pre style="background: none">${hljs.highlightAuto(code).value}</pre>`
 }
