7feba74c79bd140a257806f7800643a055c29889
Nine9 <germmc99@gmail.com>
2023-03-06 23:16:43 +0800

    feat: 增加聊天记录导入导出功能

--- src/components/common/Setting/General.vue
+++ src/components/common/Setting/General.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { computed, ref } from 'vue'
-import { NButton, NInput, useMessage } from 'naive-ui'
+import { NButton, NInput, NPopconfirm, useMessage } from 'naive-ui'
 import type { Language, Theme } from '@/store/modules/app/helper'
 import { SvgIcon } from '@/components/common'
 import { useAppStore, useUserStore } from '@/store'
@@ -70,6 +70,57 @@ function handleReset() {
   ms.success(t('common.success'))
   emit('update')
 }
+
+function exportData(): void {
+  const data: string = localStorage.getItem('chatStorage') || '{}'
+
+  const jsonString: string = JSON.stringify(JSON.parse(data), null, 2)
+
+  const blob: Blob = new Blob([jsonString], { type: 'application/json' })
+  const url: string = URL.createObjectURL(blob)
+  const link: HTMLAnchorElement = document.createElement('a')
+  link.href = url
+  link.download = 'chat-store.json'
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+}
+
+function importData(event: Event): void {
+  const target = event.target as HTMLInputElement
+  if (!target || !target.files)
+    return
+
+  const file: File = target.files[0]
+  if (!file)
+    return
+
+  const reader: FileReader = new FileReader()
+  reader.onload = () => {
+    try {
+      const data = JSON.parse(reader.result as string)
+      localStorage.setItem('chatStorage', JSON.stringify(data))
+      ms.success(t('common.success'))
+      emit('update')
+      location.reload()
+    }
+    catch (error) {
+      ms.error(t('common.invalidFileFormat'))
+    }
+  }
+  reader.readAsText(file)
+}
+
+function clearData(): void {
+  localStorage.removeItem('chatStorage')
+  location.reload()
+}
+
+function handleImportButtonClick(): void {
+  const fileInput = document.getElementById('fileInput') as HTMLElement
+  if (fileInput)
+    fileInput.click()
+}
 </script>
 
 <template>
@@ -107,6 +158,26 @@ function handleReset() {
         <NButton text type="primary" @click="handleReset">
           {{ $t('common.reset') }}
         </NButton>
+      </div>
+      <div class="flex items-center space-x-4">
+        <span class="flex-shrink-0 w-[100px]">{{ $t('setting.chatHistory') }}</span>
+        <NButton text type="primary" @click="exportData">
+          {{ $t('common.export') }}
+        </NButton>
+
+        <input id="fileInput" type="file" style="display:none" @change="importData">
+        <NButton text type="primary" @click="handleImportButtonClick">
+          {{ $t('common.import') }}
+        </NButton>
+
+        <NPopconfirm placement="bottom" @positive-click="clearData">
+          <template #trigger>
+            <NButton text type="primary">
+              {{ $t('common.clear') }}
+            </NButton>
+          </template>
+          {{ $t('chat.clearHistoryConfirm') }}
+        </NPopconfirm>
       </div>
       <div class="flex items-center space-x-4">
         <span class="flex-shrink-0 w-[100px]">{{ $t('setting.theme') }}</span>
