107415227964655ed18f56767d2bb424199663cd
chenxch <124118265@qq.com>
2023-03-10 01:05:21 +0800

    feat: support pwa

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,14 @@ export default defineConfig((env) => {
       vue(),
       VitePWA({
         injectRegister: 'auto',
+        manifest: {
+          name: 'chatGPT',
+          short_name: 'chatGPT',
+          icons: [
+            { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+            { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
+          ],
+        },
       }),
     ],
     server: {
