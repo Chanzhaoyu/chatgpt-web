c015497bdaa537e5da9463cc69dcc384f9efb1ce
Shan Wu <Shan.Wu@ut.ee>
2023-03-11 23:53:16 +0200

    fix(PromptStore): import prompt template using different dict keys

--- src/components/common/PromptStore/index.vue
+++ src/components/common/PromptStore/index.vue
@@ -144,22 +144,39 @@ const clearPromptTemplate = () => {
 const importPromptTemplate = () => {
   try {
     const jsonData = JSON.parse(tempPromptValue.value)
+    let key = ''
+    let value = ''
+    // 可以扩展加入更多模板字典的key
+    if ('key' in jsonData[0]) {
+      key = 'key'
+      value = 'value'
+    }
+    else if ('act' in jsonData[0]) {
+      key = 'act'
+      value = 'prompt'
+    }
+    else {
+      // 不支持的字典的key防止导入 以免破坏prompt商店打开
+      message.warning('prompt key not supported.')
+      throw new Error('prompt key not supported.')
+    }
+
     for (const i of jsonData) {
       let safe = true
       for (const j of promptList.value) {
-        if (j.key === i.key) {
-          message.warning(`因标题重复跳过：${i.key}`)
+        if (j.key === i[key]) {
+          message.warning(`因标题重复跳过：${i[key]}`)
           safe = false
           break
         }
-        if (j.value === i.value) {
-          message.warning(`因内容重复跳过：${i.key}`)
+        if (j.value === i[value]) {
+          message.warning(`因内容重复跳过：${i[key]}`)
           safe = false
           break
         }
       }
       if (safe)
-        promptList.value.unshift({ key: i.key, value: i.value } as never)
+        promptList.value.unshift({ key: i[key], value: i[value] } as never)
     }
     message.success('导入成功')
     changeShowModal('')
