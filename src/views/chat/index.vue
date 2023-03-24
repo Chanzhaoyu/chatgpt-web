fb6d4e3614d24ad796fc2867999452017e8b2eb2
RootFTW <108767514+Root-FTW@users.noreply.github.com>
2023-03-23 22:06:49 -0700

    Update index.vue

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -486,7 +486,7 @@ onUnmounted(() => {
           <template v-if="!dataSources.length">
             <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
               <SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" />
-              <span>Aha~</span>
+              <span>EvaGPT</span>
             </div>
           </template>
           <template v-else>
