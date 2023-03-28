c5ec9ef56e4d545f182573fb924bf8d42c8c0ec9
pretenderlu <luqingqin@live.com>
2023-03-28 21:01:34 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://raw.githubusercontent.com/pretenderlu/chatgpt-web/main/src/assets/avatar.jpg',
+      name: 'LuQQ',
+      description: '聊天小机器人',
     },
   }
 }
