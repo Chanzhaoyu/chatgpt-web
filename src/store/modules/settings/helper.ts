19844c299be37cd67d721c3dcff80b46cb4bb350
assassinliujie <68693675+assassinliujie@users.noreply.github.com>
2023-03-29 14:39:44 +0800

    Update helper.ts

--- src/store/modules/settings/helper.ts
+++ src/store/modules/settings/helper.ts
@@ -9,7 +9,7 @@ export interface SettingsState {
 export function defaultSetting(): SettingsState {
   const currentDate = new Date().toISOString().split('T')[0]
   return {
-    systemMessage: `You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible.\nKnowledge cutoff: 2021-09-01\nCurrent date: ${currentDate}`,
+    systemMessage: `You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible.\nCurrent date: ${currentDate}`,
   }
 }
 
