f31b612d4c8cc705ddb47777adc0278845a9c96e
assassinliujie <68693675+assassinliujie@users.noreply.github.com>
2023-03-29 03:11:18 +0000

    modified:   service/src/index.ts

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import * as tencentcloud from 'tencentcloud-sdk-nodejs'
 import type { RequestProps } from './types'
 import type { ChatMessage } from './chatgpt'
 import { chatConfig, chatReplyProcess, currentModel } from './chatgpt'
@@ -6,6 +7,7 @@ import { auth } from './middleware/auth'
 import { limiter } from './middleware/limiter'
 import { isNotEmptyString } from './utils/is'
 
+const CvmClient = tencentcloud.cvm.v20170312.Client
 const app = express()
 const router = express.Router()
 
@@ -19,11 +21,77 @@ app.all('*', (_, res, next) => {
   next()
 })
 
+// import TencentCloud SDK for NLP
+const tencentcloud = require('tencentcloud-sdk-nodejs-nlp')
+const NlpClient = tencentcloud.nlp.v20190408.Client
+const clientConfig = {
+  credential: {
+    secretId: 'YOUR_SECRET_ID',
+    secretKey: 'YOUR_SECRET_KEY',
+  },
+  region: 'ap-guangzhou',
+  profile: {
+    httpProfile: {
+      endpoint: 'nlp.tencentcloudapi.com',
+    },
+  },
+}
+const client = new NlpClient(clientConfig)
+const { google } = require('googleapis')
+
+// Instantiate a Tencent Cloud NLP client
+const nlpClient = new tencentcloud.nlp.v20190408.Client({
+  credential: {
+    secretId: 'YOUR_SECRET_ID',
+    secretKey: 'YOUR_SECRET_KEY',
+  },
+  region: 'ap-guangzhou',
+  profile: {
+    httpProfile: {
+      endpoint: 'nlp.tencentcloudapi.com',
+    },
+  },
+})
+
+// Function to search Google with provided query
+async function searchGoogle(query) {
+  const customsearch = google.customsearch('v1')
+  const res = await customsearch.cse.list({
+    cx: 'YOUR_CSE_ID', // Your Custom Search Engine ID
+    q: query,
+    auth: 'YOUR_API_KEY', // Your API Key
+  })
+  const searchResults = res.data.items.map(({ title, link }) => ({ title, link }))
+  return searchResults
+}
+
 router.post('/chat-process', [auth, limiter], async (req, res) => {
   res.setHeader('Content-type', 'application/octet-stream')
 
   try {
-    const { prompt, options = {}, systemMessage } = req.body as RequestProps
+    let { prompt, options = {}, systemMessage } = req.body as RequestProps
+
+    // Extract keywords with Tencent Cloud NLP API
+    const keywordMatch = prompt.match(/^(.*?)\b(.*?)(怎么样|\?)$/i)
+    if (keywordMatch && keywordMatch[2]) {
+      const keyword = keywordMatch[2].trim()
+      const params = {
+        Text: keyword,
+        Num: 1, // extract only one keyword
+      }
+      const response = await nlpClient.KeywordsExtraction(params)
+
+      if (response?.Keywords?.length > 0) {
+        const searchResult = response.Keywords[0].Word
+
+        // Search Google with extracted keywords
+        const searchPrompt = `${keyword} ${searchResult}`
+        const searchResults = await searchGoogle(searchPrompt)
+        const searchResultTitles = searchResults.map(result => result.title).join(', ')
+
+        prompt += `（根据搜索：${searchResultTitles}）`
+      }
+    }
     let firstChunk = true
     let chatLength = 0
     let newChatLength = 0
@@ -45,7 +113,15 @@ router.post('/chat-process', [auth, limiter], async (req, res) => {
     })
   }
   catch (error) {
-    res.write(JSON.stringify(error))
+    console.error(error)
+    let errorMessage = 'Server Error'
+    if (error?.code === 'InvalidParameterValue')
+      errorMessage = '请求参数错误 | Invalid request parameter'
+
+    else if (error?.message)
+      errorMessage = error.message
+
+    res.status(500).send({ status: 'Fail', message: errorMessage, data: null })
   }
   finally {
     res.end()
@@ -58,7 +134,8 @@ router.post('/config', auth, async (req, res) => {
     res.send(response)
   }
   catch (error) {
-    res.send(error)
+    console.error(error)
+    res.status(500).send({ status: 'Fail', message: error.message, data: null })
   }
 })
 
@@ -69,7 +146,8 @@ router.post('/session', async (req, res) => {
     res.send({ status: 'Success', message: '', data: { auth: hasAuth, model: currentModel() } })
   }
   catch (error) {
-    res.send({ status: 'Fail', message: error.message, data: null })
+    console.error(error)
+    res.status(500).send({ status: 'Fail', message: error.message, data: null })
   }
 })
 
