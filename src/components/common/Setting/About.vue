c8aea819f0d204f76d11dee5a1b59df32e575069
allen <103651858+leealu@users.noreply.github.com>
2023-03-31 05:25:29 +0800

    Update About.vue

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -49,16 +49,14 @@ onMounted(() => {
           此项目开源于
           <a
             class="text-blue-600 dark:text-blue-500"
-            href="https://github.com/Chanzhaoyu/chatgpt-web"
+            href="https://github.com/leealu/chatgpt-web"
             target="_blank"
           >
             Github
           </a>
           ，免费且基于 MIT 协议，没有任何形式的付费行为！
         </p>
-        <p>
-          如果你觉得此项目对你有帮助，请在 Github 帮我点个 Star 或者给予一点赞助，谢谢！
-        </p>
+
       </div>
       <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
       <p v-if="isChatGPTAPI">
