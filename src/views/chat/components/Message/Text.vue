9bd88eac844625d4f743838e84c8923cd8c42cf3
ChenZhaoYu <790348264@qq.com>
2023-03-31 14:52:54 +0800

    fix: 宽度问题

--- src/views/chat/components/Message/Text.vue
+++ src/views/chat/components/Message/Text.vue
@@ -67,8 +67,8 @@ defineExpose({ textRef })
   <div class="text-black" :class="wrapClass">
     <div ref="textRef" class="leading-relaxed break-words">
       <div v-if="!inversion" class="flex items-end">
-        <div v-if="!asRawText" class="markdown-body" v-html="text" />
-        <div v-else class="whitespace-pre-wrap" v-text="text" />
+        <div v-if="!asRawText" class="w-full markdown-body" v-html="text" />
+        <div v-else class="w-full whitespace-pre-wrap" v-text="text" />
         <span v-if="loading" class="dark:text-white w-[4px] h-[20px] block animate-blink" />
       </div>
       <div v-else class="whitespace-pre-wrap" v-text="text" />
