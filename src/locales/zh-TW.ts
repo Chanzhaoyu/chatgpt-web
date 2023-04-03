a02b515ed93fcc565581be0fbb6fb168b250edbd
RyanXin <xyz@ryanxin.cn>
2023-04-03 11:11:13 +0100

    chore: 引导用户触发提示词

--- src/locales/zh-TW.ts
+++ src/locales/zh-TW.ts
@@ -29,7 +29,7 @@ export default {
   },
   chat: {
     newChatButton: '新建對話',
-    placeholder: '來說點什麼...（Shift + Enter = 換行）',
+    placeholder: '來說點什麼...（Shift + Enter = 換行，"/" 觸發提示詞）',
     placeholderMobile: '來說點什麼...',
     copy: '複製',
     copied: '複製成功',
