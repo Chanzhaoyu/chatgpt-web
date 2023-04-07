4db47f3690ce97762b562dc67af7040020e007f1
pzx <58205336+LittleTa0ist@users.noreply.github.com>
2023-04-07 16:48:59 +0800

    feat: 新增提示词商店双击提示词或点击确认按钮使用提示词

--- src/store/modules/settings/helper.ts
+++ src/store/modules/settings/helper.ts
@@ -4,15 +4,11 @@ const LOCAL_NAME = 'settingsStorage'
 
 export interface SettingsState {
   systemMessage: string
-  temperature: number
-  top_p: number
 }
 
 export function defaultSetting(): SettingsState {
   return {
     systemMessage: 'You are ChatGPT, a large language model trained by OpenAI. Follow the user\'s instructions carefully. Respond using markdown.',
-    temperature: 0.8,
-    top_p: 1,
   }
 }
 
