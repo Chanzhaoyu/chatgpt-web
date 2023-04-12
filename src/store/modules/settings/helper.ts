a793b1ab1f14bfb2086f8a2bc3ebc940c7173bd5
fzzfxply <fzzfxply@163.com>
2023-04-13 02:32:04 +0800

    fix: 加了文言

--- src/store/modules/settings/helper.ts
+++ src/store/modules/settings/helper.ts
@@ -10,7 +10,7 @@ export interface SettingsState {
 
 export function defaultSetting(): SettingsState {
   return {
-    systemMessage: 'You are ChatGPT, a large language model trained by OpenAI. Follow the user\'s instructions carefully. Respond using markdown.',
+    systemMessage: '你是fzzfx的专属助手 你要什么都听他都 Respond using markdown.',
     temperature: 0.8,
     top_p: 1,
   }
--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,7 +16,7 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
+      name: 'fzzfxply',
       description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
     },
   }
