e6ff7cdcfa345d9d9dfce6e18b2fd7fe9554ee8a
tiantianlike <71114001+tiantianlike@users.noreply.github.com>
2023-04-12 14:02:41 +0800

    Add files via upload

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://btj-image.oss-cn-zhangjiakou.aliyuncs.com/TouImg/toubtj.png',
+      name: 'btj',
+      description: 'Star on <a href="https://etsc.xyz/" class="text-blue-500" target="_blank" >Github</a>',
     },
   }
 }
