411baaa99992ea474ebcfa6d9a1da98450ce8f97
Peter Dave Hello <hsu@peterdavehello.org>
2023-04-15 03:58:59 +0800

    Let's make GitHub happy

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -17,7 +17,7 @@ export function defaultSetting(): UserState {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
       name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
