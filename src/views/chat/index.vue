f194a4b3fded2e43559fb01011451d0d83bd827f
刘建超 <liujianchao1995@qq.com>
2023-04-18 23:50:41 +0800

    fix: 修改默认信息。

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -484,7 +484,7 @@ onUnmounted(() => {
           <template v-if="!dataSources.length">
             <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
               <SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" />
-              <span>Aha~</span>
+              <span>(●'◡'●)</span>
             </div>
           </template>
           <template v-else>
@@ -505,7 +505,7 @@ onUnmounted(() => {
                   <template #icon>
                     <SvgIcon icon="ri:stop-circle-line" />
                   </template>
-                  Stop Responding
+                  停止回答
                 </NButton>
               </div>
             </div>
