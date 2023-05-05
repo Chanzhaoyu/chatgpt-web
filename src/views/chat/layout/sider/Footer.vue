fafe30036dbb7c6c9cdffb3f64555026e4003651
裴侃 <peikang2015@outlook.com>
2023-05-05 19:11:07 +0800

    qcCode

--- src/views/chat/layout/sider/Footer.vue
+++ src/views/chat/layout/sider/Footer.vue
@@ -1,24 +1,25 @@
 <script setup lang='ts'>
 import { defineAsyncComponent, ref } from 'vue'
 import { HoverButton, SvgIcon, UserAvatar } from '@/components/common'
-
+import QcCode from '@/assets/QcCode.jpg'
 const Setting = defineAsyncComponent(() => import('@/components/common/Setting/index.vue'))
 
 const show = ref(false)
 </script>
 
 <template>
-  <footer class="flex items-center justify-between min-w-0 p-4 overflow-hidden border-t dark:border-neutral-800">
-    <div class="flex-1 flex-shrink-0 overflow-hidden">
-      <UserAvatar />
-    </div>
-
-    <HoverButton @click="show = true">
+	<div style="height: 120px;border-top: 1px solid #eeeeee" class="items-center justify-center p-1">
+		<img :src="QcCode" style="height:100%" class="m-auto w-auto">
+	</div>
+	<footer class="flex items-center justify-between min-w-0 p-4 overflow-hidden border-t dark:border-neutral-800">
+		<div class="flex-1 flex-shrink-0 overflow-hidden">
+			<UserAvatar />
+		</div>
+		<HoverButton @click="show = true">
       <span class="text-xl text-[#4f555e] dark:text-white">
         <SvgIcon icon="ri:settings-4-line" />
       </span>
-    </HoverButton>
-
-    <Setting v-if="show" v-model:visible="show" />
-  </footer>
+		</HoverButton>
+		<Setting v-if="show" v-model:visible="show" />
+	</footer>
 </template>
