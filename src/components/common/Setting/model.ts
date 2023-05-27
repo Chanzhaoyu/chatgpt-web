2085d90c06e170927e693191f09ffe8911db86f1
Kerwin <me@zhanghuanglong.com>
2023-05-27 18:37:46 +0800

    chore: 增加用户组 (Close #180)

--- src/components/common/Setting/model.ts
+++ src/components/common/Setting/model.ts
@@ -76,6 +76,12 @@ export enum UserRole {
   Admin = 0,
   User = 1,
   Guest = 2,
+  Support = 3,
+  Viewer = 4,
+  Contributor = 5,
+  Developer = 6,
+  Tester = 7,
+  Partner = 8,
 }
 
 export class KeyConfig {
