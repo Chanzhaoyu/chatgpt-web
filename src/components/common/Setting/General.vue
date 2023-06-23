2191b06a4c247b6e752130a6834467dd2d54c881
rasta26 <109227276+rasta26@users.noreply.github.com>
2023-06-22 21:00:01 -0400

    Update General.vue

--- src/components/common/Setting/General.vue
+++ src/components/common/Setting/General.vue
@@ -57,6 +57,7 @@ const languageOptions: { label: string; key: Language; value: Language }[] = [
   { label: '简体中文', key: 'zh-CN', value: 'zh-CN' },
   { label: '繁體中文', key: 'zh-TW', value: 'zh-TW' },
   { label: 'English', key: 'en-US', value: 'en-US' },
+	{ label: 'Spanish', key: 'es-ES', value: 'es-AR' },
   { label: '한국어', key: 'ko-KR', value: 'ko-KR' },
   { label: 'Русский язык', key: 'ru-RU', value: 'ru-RU' },
 ]
