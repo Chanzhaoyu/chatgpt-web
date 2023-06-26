ade708927577eba53098e8a631bd403b44423545
rasta26 <109227276+rasta26@users.noreply.github.com>
2023-06-26 10:17:49 -0400

    Update helper.ts

--- src/store/modules/app/helper.ts
+++ src/store/modules/app/helper.ts
@@ -4,7 +4,7 @@ const LOCAL_NAME = 'appSetting'
 
 export type Theme = 'light' | 'dark' | 'auto'
 
-export type Language = 'zh-CN' | 'zh-TW' | 'en-US' | 'esp-ES' |'ko-KR' | 'ru-RU'
+export type Language = 'zh-CN' | 'zh-TW' | 'en-US' | 'es-ES' |'ko-KR' | 'ru-RU'
 
 export interface AppState {
   siderCollapsed: boolean
