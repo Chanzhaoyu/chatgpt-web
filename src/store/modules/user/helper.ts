2a87abdf2395f5b9b7c661bff021c2b302a21e34
赵瑞阳 <zhaoruiyang@icourt.cc>
2023-08-11 14:03:18 +0800

    fix: edit user info

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      name: 'coderzry',
+      description: '',
     },
   }
 }
