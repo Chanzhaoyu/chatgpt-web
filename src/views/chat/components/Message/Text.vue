ab9dca471d1a68bcac62034c61b7fbf3ce7ff8a9
ChenZhoYu <790348264@qq.com>
2024-05-16 09:38:50 +0800

    perf: change katex package

--- src/views/chat/components/Message/Text.vue
+++ src/views/chat/components/Message/Text.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { computed, onMounted, onUnmounted, onUpdated, ref } from 'vue'
 import MarkdownIt from 'markdown-it'
-import mdKatex from '@traptitech/markdown-it-katex'
+import mdKatex from '@vscode/markdown-it-katex'
 import mila from 'markdown-it-link-attributes'
 import hljs from 'highlight.js'
 import { useBasicLayout } from '@/hooks/useBasicLayout'
@@ -36,7 +36,7 @@ const mdi = new MarkdownIt({
 })
 
 mdi.use(mila, { attrs: { target: '_blank', rel: 'noopener' } })
-mdi.use(mdKatex, { blockClass: 'katexmath-block rounded-md p-[10px]', errorColor: ' #cc0000' })
+mdi.use(mdKatex)
 
 const wrapClass = computed(() => {
   return [
