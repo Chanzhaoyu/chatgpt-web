f257a546047359ecaf1b0336f1412ecb46ee3b43
Peter Dave Hello <hsu@peterdavehello.org>
2024-05-16 09:39:49 +0800

    feat: add GPT-4o support (#2082)
    Reference:
    - https://platform.openai.com/docs/models/gpt-4o
    - https://openai.com/index/hello-gpt-4o/

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -53,8 +53,8 @@ let api: ChatGPTAPI | ChatGPTUnofficialProxyAPI
         options.maxModelTokens = 32768
         options.maxResponseTokens = 8192
       }
-      // if use GPT-4 Turbo
-      else if (/-preview|-turbo/.test(model.toLowerCase())) {
+      // if use GPT-4 Turbo or GPT-4o
+      else if (/-preview|-turbo|-o/.test(model.toLowerCase())) {
         options.maxModelTokens = 128000
         options.maxResponseTokens = 4096
       }
